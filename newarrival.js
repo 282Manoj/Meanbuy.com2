5ba2cebe4e664be1f450dbf6fc59b06045492e5e
Mano Kumar <manojyad436@gmail.com>
2022-06-19 15:39:09 +0530

    links updated

--- newarrival.js
+++ newarrival.js
@@ -1,156 +1,68 @@
 let newarrival = [
   {
+    img:"https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxOTc2L0JsYWNrX01fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
+    name:"Summer Cottion Losse Dress",
+    rating:"4.5/5★",
+    price:"₹899",
+    offer:"UP TO 55% OFF",
+    catagory:"cottion",
+    brand:"",
+},
+ {
+    img:"https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxOTc3L0JsYWNrX1NfMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
+    name:"Retro Neck Casual Tops",
+    rating:"3.7/5★",
+    price:"₹1,399",
+    offer:"UP TO 39% OFF",
+    catagory:"Casual",
+    brand:"",
+},
+ {
+    img:"https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxOTcwL0JsYWNrX1NfMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
+    name:"Summer Loose Casual Dress",
+    rating:"4.7/5★",
+    price:"₹1,899",
+    offer:"UP TO 37% OFF",
+    catagory:"Casual",
+    brand:"",
+},
 
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
-  {
-
-    img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
-    name: "True Wireless Dynamic Earbuds",
-    rating: "4.5/5 ★",
-    price: "₹1,999",
-    offer: "UP TO 35% OFF",
-  },
+ {
+    img:"https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxNzQxL0Fwcmljb3Rfbl8xXzEuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
+    name:"Free Size Summer Tops",
+    rating:"4.7/5★",
+    price:"₹2,499",
+    offer:"UP TO 50% OFF",
+    catagory:"Summer",
+    brand:"",
+},
+ {
+    img:"https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxNzI1L0JsdWVfbl8xXzEuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
+    name:"Women Retro T-shirt",
+    rating:"4.0/5★",
+    price:"₹899",
+    offer:"UP TO 50% OFF",
+    catagory:"retr0",
+    brand:"",
+},
+ {
+    img:"https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxNzI2L0Jyb3duX25fMV8xLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MzIwLCJoZWlnaHQiOjMyMCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9fX19",
+    name:"Women V-neck Bat T-shirt",
+    rating:"4.5/5★",
+    price:"₹1,999",
+    offer:"UP TO 50% OFF",
+    catagory:"t-shirt",
+    brand:"",
+},
+ {
+    img:"https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjE5NzEzL05hdnkrQmx1ZV9uXzFfMS5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjMyMCwiaGVpZ2h0IjozMjAsImZpdCI6ImNvbnRhaW4iLCJiYWNrZ3JvdW5kIjp7InIiOjI1NSwiZyI6MjU1LCJiIjoyNTUsImFscGhhIjoxfX19fQ==",
+    name:"Women Patchwork Elasric Waisr Skirt",
+    rating:"4.7/5★",
+    price:"₹1,699",
+    offer:"UP TO 23% OFF",
+    catagory:"skirt",
+    brand:"",
+},
   {
 
     img: "https://dbnzj30o7hucx.cloudfront.net/eyJidWNrZXQiOiJtZWFuYnV5LWltYWdlcyIsImtleSI6IjMxMzQ5L0dyYXlfbl8xXzEucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjozMjAsImhlaWdodCI6MzIwLCJmaXQiOiJjb250YWluIiwiYmFja2dyb3VuZCI6eyJyIjoyNTUsImciOjI1NSwiYiI6MjU1LCJhbHBoYSI6MX19fX0=",
@@ -186,4 +98,11 @@ function display(newarrival) {
     box3.append(box, box2);
     document.querySelector("#main").append(box3);
   });
+  document.querySelector("#cart").addEventListener("click",function(){
+    window.location.href="cart.html";
+  })
+  document.querySelector("#logo").addEventListener("click",function(){
+    window.location.href="nav+home.html";
+  })
+  
 }
